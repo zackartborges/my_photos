da4ff56726f8a0ea2a885d73f7a556f592a892dc
Zack Borges <zackartborges@gmail.com>
2021-05-20 18:50:54 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- Heading-->
+    <div id="title">
+      <h1>I am Zack Borges</h1>
+      <h3>This is my personal website.</h3>
+    </div>
+    <hr />
+    <!-- Body -->
+    <div id="upper-body">
+      <p>
+        I decided to practice my web development basics skills, by creating my own website. This is meant to serve as a
+        journal for me, as well as a place where I can be referenced. As you can tell, I am very new to this, but bear
+        with me. Please.. you are my only hope.
+      </p>
+      <hr />
+    </div>
+    <!-- Left Images -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
